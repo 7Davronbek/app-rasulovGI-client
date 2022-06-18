5ce941e74e197574af492742dc5ecf255e9bc089
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-18 12:07:33 +0500

    rasulovGI-app

--- src/components/Katalog.jsx
+++ src/components/Katalog.jsx
@@ -9,21 +9,38 @@ import "swiper/css/pagination";
 import { Pagination } from "swiper";
 
 const Katalog = () => {
-  return (
-    <div className='Katalog'>
-        <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-      </Swiper>
-    </div>
-  )
+    return (
+        <div className='Katalog'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1>Каталог</h1>
+                    </div>
+                    <div className="col-12">
+                        <Swiper
+                            slidesPerView={3}
+                            spaceBetween={30}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            modules={[Pagination]}
+                            className="mySwiper"
+                        >
+                            <SwiperSlide>
+                                <div className="cards">
+                                    <img src="/img/catalog/1.png" className='w-100' alt="" />
+                                    <h1>Эксклюзивный класс</h1>
+
+                                    <p>Эксклюзивом принято называть то, что доступно не каждому. Вы сделали или планируете сделать интерьер своего жилища выходящим за рамки усредненных требований и стандартов – элитным, престижным, дорогим? Тогда у вас обязательно должны быть установлены эксклюзивные межкомнатные двери.</p>
+                                    <a href="#!">Ознакомиться с товаром <span><img src="/icon/arrowRight.svg" alt="" /></span></a>
+                                </div>
+                            </SwiperSlide>
+                        </Swiper>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Katalog
