6f82ee937228ceb8a3c2998844a8d403f6f5889e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-20 14:10:29 +0500

    practise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Main from "./pages/Main";
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      console.clear();
+      console.log('Instagram: @duvronbekmaxmudov');
+    }, 2000);
+  }, []);
   return (
     <>
       <Navbar />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,21 @@ const Footer = () => {
     <div className='footer'>
         <div className="container">
             <div className="row">
-                <div className="col-12">
+                <div className="col-12 zed">
                     <h1>Контакты</h1>
+                </div>
+                <div className="col-4 zed">
+                    <div className="cards">
+                        <div><img src="/icon/phone.svg" alt="" /></div>
+                        <div>
+                            <a href="tel: +998943698058">+998(90) 123-45-67</a>
+                            <a href="tel: +998943698058">+998(90) 123-45-67</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <div className="blur"></div>
     </div>
   )
 }
