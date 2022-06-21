d6e56d2bb0bd61e609232eadae31020897597295
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-21 09:38:32 +0500

    rasulovGI-app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='Header'>
-        
+      <div className="container">
+        <div className="row">
+          
+        </div>
+      </div>
     </div>
   )
 }
