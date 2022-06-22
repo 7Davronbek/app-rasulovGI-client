103c6d19b386a525574d115b9ffd25487912a51d
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-22 17:36:13 +0500

    rasulovGI-app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import AwesomeSlider from 'react-awesome-slider';
+// import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+// import AwesomeSliderStyles from 'react-awesome-slider/src/styles';
+import CoreStyles from 'react-awesome-slider/src/core/styles.scss';
+import AnimationStyles from 'react-awesome-slider/src/styled/fold-out-animation/fold-out-animation.scss';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+
 
 const Header = () => {
   return (
@@ -6,11 +16,22 @@ const Header = () => {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor enim non aperiam corporis officiis aspernatur mollitia eos, expedita, at eligendi debitis obcaecati laborum dignissimos esse placeat. Quidem nulla quod nisi.
+
+            <AutoplaySlider
+              play={true}
+              animation="foldOutAnimation"
+              cssModule={[CoreStyles, AnimationStyles]}
+              cancelOnInteraction={false} // should stop playing on user interaction
+              interval={6000}>
+              <div data-src="/img/header.png" />
+              <div data-src="/img/header.png" />
+              <div data-src="/img/header.png" />
+            </AutoplaySlider>
+
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
