4e906b37ccb93b17f73a97107839cff3919a0ffe
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-24 17:38:16 +0500

    nextlevel-app

--- src/components/CatalogContent.jsx
+++ src/components/CatalogContent.jsx
@@ -7,7 +7,7 @@ const CatalogContent = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-4">
-                            <div className="cards">
+                            <div className="cardsLeft">
                                 <div className="searchWrap d-flex align-items-center justify-content-between ">
                                     <input type="text" className='form-control' placeholder='Поск по сайту' />
                                     <span><img src="/icon/zoom.svg" alt="" /></span>
@@ -16,6 +16,47 @@ const CatalogContent = () => {
                                 <a href="#!">Эксклюзивнйы класс</a>
                                 <a href="#!">Экошпон</a>
                                 <a href="#!">Премиум класс</a>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-8">
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <div className="cards">
+                                        <img src="/img/catalog/1.png" className='w-100' alt="" />
+                                        <h2>Эксклюзивный класс</h2>
+
+                                        <p>Эксклюзивом принято называть то, что доступно не каждому. Вы сделали или планируете сделать интерьер своего жилища выходящим за рамки усредненных требований и стандартов – элитным, престижным, дорогим? Тогда у вас обязательно должны быть установлены эксклюзивные межкомнатные двери.</p>
+                                        <a href="#!">Ознакомиться с товаром <span><img src="/icon/arrowRight.svg" alt="" /></span></a>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="cards">
+                                        <img src="/img/catalog/1.png" className='w-100' alt="" />
+                                        <h2>Эксклюзивный класс</h2>
+
+                                        <p>Эксклюзивом принято называть то, что доступно не каждому. Вы сделали или планируете сделать интерьер своего жилища выходящим за рамки усредненных требований и стандартов – элитным, престижным, дорогим? Тогда у вас обязательно должны быть установлены эксклюзивные межкомнатные двери.</p>
+                                        <a href="#!">Ознакомиться с товаром <span><img src="/icon/arrowRight.svg" alt="" /></span></a>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="cards">
+                                        <img src="/img/catalog/1.png" className='w-100' alt="" />
+                                        <h2>Эксклюзивный класс</h2>
+
+                                        <p>Эксклюзивом принято называть то, что доступно не каждому. Вы сделали или планируете сделать интерьер своего жилища выходящим за рамки усредненных требований и стандартов – элитным, престижным, дорогим? Тогда у вас обязательно должны быть установлены эксклюзивные межкомнатные двери.</p>
+                                        <a href="#!">Ознакомиться с товаром <span><img src="/icon/arrowRight.svg" alt="" /></span></a>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="cards">
+                                        <img src="/img/catalog/1.png" className='w-100' alt="" />
+                                        <h2>Эксклюзивный класс</h2>
+
+                                        <p>Эксклюзивом принято называть то, что доступно не каждому. Вы сделали или планируете сделать интерьер своего жилища выходящим за рамки усредненных требований и стандартов – элитным, престижным, дорогим? Тогда у вас обязательно должны быть установлены эксклюзивные межкомнатные двери.</p>
+                                        <a href="#!">Ознакомиться с товаром <span><img src="/icon/arrowRight.svg" alt="" /></span></a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
