e5faa1a1023ac180e6e6e169e8de272c29618f5e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-24 19:33:07 +0500

    rasulovGI-app

--- src/components/CatalogById.jsx
+++ src/components/CatalogById.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const CatalogById = () => {
+    const [modal, setModal] = useState(false)
     return (
         <>
             <div className="CatalogById">
@@ -27,17 +28,92 @@ const CatalogById = () => {
                         <div className="col-lg-8">
                             <div className="row">
                                 <div className="col-lg-6">
-                                    <div className="cards">
-                                        <img src="/img/catalog/1.png" className='w-100' alt="" />
-                                        <h2>Эксклюзивный класс</h2>
-
-                                        <p>Эксклюзивом принято называть то, что доступно не каждому. Вы сделали или планируете сделать интерьер своего жилища выходящим за рамки усредненных требований и стандартов – элитным, престижным, дорогим? Тогда у вас обязательно должны быть установлены эксклюзивные межкомнатные двери.</p>
-                                        <Link to='/catalog/by-id'>Ознакомиться с товаром <span><img src="/icon/arrowRight.svg" alt="" /></span></Link>
+                                    <div onClick={() => setModal(true)} className="cards">
+                                        <img src="/img/catalogId/1.png" className='w-100' alt="" />
+                                        <h3>Модель №1</h3>
+                                        <p>
+                                            <div className="under">Параметры:</div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Колекция</span>
+                                                <span>Элит</span>
+                                            </div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Шпон</span>
+                                                <span>Американский орех</span>
+                                            </div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Цвет</span>
+                                                <span>На выбор заказчика</span>
+                                            </div>
+                                        </p>
                                     </div>
                                 </div>
                                 <div className="col-lg-6 actives">
+                                    <div onClick={() => setModal(true)} className="cards">
+                                        <img src="/img/catalogId/2.png" className='w-100' alt="" />
+                                        <h3>Модель №1</h3>
+                                        <p>
+                                            <div className="under">Параметры:</div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Колекция</span>
+                                                <span>Элит</span>
+                                            </div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Шпон</span>
+                                                <span>Американский орех</span>
+                                            </div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Цвет</span>
+                                                <span>На выбор заказчика</span>
+                                            </div>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 ">
+                                    <div onClick={() => setModal(true)} className="cards">
+                                        <img src="/img/catalogId/3.png" className='w-100' alt="" />
+                                        <h3>Модель №1</h3>
+                                        <p>
+                                            <div className="under">Параметры:</div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Колекция</span>
+                                                <span>Элит</span>
+                                            </div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Шпон</span>
+                                                <span>Американский орех</span>
+                                            </div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Цвет</span>
+                                                <span>На выбор заказчика</span>
+                                            </div>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 actives">
+                                    <div onClick={() => setModal(true)} className="cards">
+                                        <img src="/img/catalogId/4.png" className='w-100' alt="" />
+                                        <h3>Модель №1</h3>
+                                        <p>
+                                            <div className="under">Параметры:</div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Колекция</span>
+                                                <span>Элит</span>
+                                            </div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Шпон</span>
+                                                <span>Американский орех</span>
+                                            </div>
+                                            <div className="d-flex align-items-center">
+                                                <span>Цвет</span>
+                                                <span>На выбор заказчика</span>
+                                            </div>
+                                        </p>
+                                    </div>
+                                </div>
+                                {/* <div className="col-lg-6 actives">
                                     <div className="cards">
-                                        <img src="/img/catalog/4.png" className='w-100' alt="" />
+                                        <img src="/img/catalogId/4.png" className='w-100' alt="" />
                                         <h2>Эксклюзивный класс</h2>
 
                                         <p>Эксклюзивом принято называть то, что доступно не каждому. Вы сделали или планируете сделать интерьер своего жилища выходящим за рамки усредненных требований и стандартов – элитным, престижным, дорогим? Тогда у вас обязательно должны быть установлены эксклюзивные межкомнатные двери.</p>
@@ -46,7 +122,7 @@ const CatalogById = () => {
                                 </div>
                                 <div className="col-lg-6">
                                     <div className="cards">
-                                        <img src="/img/catalog/3.png" className='w-100' alt="" />
+                                        <img src="/img/catalogId/3.png" className='w-100' alt="" />
                                         <h2>Эксклюзивный класс</h2>
 
                                         <p>Эксклюзивом принято называть то, что доступно не каждому. Вы сделали или планируете сделать интерьер своего жилища выходящим за рамки усредненных требований и стандартов – элитным, престижным, дорогим? Тогда у вас обязательно должны быть установлены эксклюзивные межкомнатные двери.</p>
@@ -55,14 +131,37 @@ const CatalogById = () => {
                                 </div>
                                 <div className="col-lg-6 actives">
                                     <div className="cards">
-                                        <img src="/img/catalog/2.png" className='w-100' alt="" />
+                                        <img src="/img/catalogId/2.png" className='w-100' alt="" />
                                         <h2>Эксклюзивный класс</h2>
 
                                         <p>Эксклюзивом принято называть то, что доступно не каждому. Вы сделали или планируете сделать интерьер своего жилища выходящим за рамки усредненных требований и стандартов – элитным, престижным, дорогим? Тогда у вас обязательно должны быть установлены эксклюзивные межкомнатные двери.</p>
                                         <Link to='/catalog/by-id'>Ознакомиться с товаром <span><img src="/icon/arrowRight.svg" alt="" /></span></Link>
                                     </div>
-                                </div>
+                                </div> */}
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={`modals  ${modal ? 'active' : ''}`}>
+                <div onClick={() => setModal(false)} className="close"><img src="/icon/close.svg" alt="" /></div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="content">
+                                <h1>Оставить заявку</h1>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <input type="text" placeholder='Имя' className='form-control' />
+                            <input type="text" placeholder='Номер телефона' className='form-control mb-0' />
+                        </div>
+                        <div className="col-lg-6">
+                            <textarea name="" cols="20" placeholder='Сообщение' className=' mb-0' ></textarea>
+                        </div>
+                        <div className="col-12">
+                            <button className="btn">Отправить</button>
                         </div>
                     </div>
                 </div>
