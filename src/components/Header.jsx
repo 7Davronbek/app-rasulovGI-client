e066e2dea55057c210425fa5a4f7df75980de4cb
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-25 15:17:00 +0500

    rasulovGI-app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,96 +9,16 @@ import "swiper/css/pagination";
 
 
 // import required modules
-import { EffectFade, Pagination, Parallax, Navigation } from "swiper";
+import { EffectFade, Pagination, } from "swiper";
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
     return (
         <div className='Header'>
-            <Swiper
-                style={{
-                    "--swiper-navigation-color": "#fff",
-                    "--swiper-pagination-color": "#fff",
-                }}
-                speed={600}
-                parallax={true}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Parallax, Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <div
-                    slot="container-start"
-                    className="parallax-bg"
-                    style={{
-                        "background-image":
-                            "url(https://swiperjs.com/demos/images/nature-1.jpg)",
-                    }}
-                    data-swiper-parallax="-23%"
-                ></div>
-                <SwiperSlide>
-                    <div className="title" data-swiper-parallax="-300">
-                        Slide 1
-                    </div>
-                    <div className="subtitle" data-swiper-parallax="-200">
-                        Subtitle
-                    </div>
-                    <div className="text" data-swiper-parallax="-100">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                            dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-                            laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-                            Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-                            Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-                            ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-                            tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-                        </p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="title" data-swiper-parallax="-300">
-                        Slide 2
-                    </div>
-                    <div className="subtitle" data-swiper-parallax="-200">
-                        Subtitle
-                    </div>
-                    <div className="text" data-swiper-parallax="-100">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                            dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-                            laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-                            Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-                            Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-                            ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-                            tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-                        </p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="title" data-swiper-parallax="-300">
-                        Slide 3
-                    </div>
-                    <div className="subtitle" data-swiper-parallax="-200">
-                        Subtitle
-                    </div>
-                    <div className="text" data-swiper-parallax="-100">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                            dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-                            laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-                            Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-                            Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-                            ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-                            tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-                        </p>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
 
 
-            {/* 
+
             <Swiper
                 spaceBetween={30}
                 effect={"fade"}
@@ -118,8 +38,8 @@ const Header = () => {
                                     <span className='bg'><img src="/img/bg.png" alt="" /></span>
                                     <div className="cardsBody">
                                         <h1>Orci nullam aliquet.</h1>
-                                        <h2>Уют начинается с нас! </h2>
-                                        <a href="#!">Orci nullam aliquet. <span><img src="/icon/arrowRight.svg" alt="" /></span></a>
+                                        <h2>ОТКРОЙ ДВЕРЬ В НОВУЮ ЖИЗНЬ !</h2>
+                                        <Link to='/catalog'>Подробнее <span><img src="/icon/arrowRight.svg" alt="" /></span></Link>
                                     </div>
                                 </div>
                             </div>
@@ -161,7 +81,7 @@ const Header = () => {
                     </div>
                 </SwiperSlide>
 
-            </Swiper> */}
+            </Swiper>
 
 
         </div>
--- src/components/Number.jsx
+++ src/components/Number.jsx
@@ -87,6 +87,8 @@ const Number = () => {
                     </div>
                 </div>
             </div>
+
+            
         </>
     )
 }
