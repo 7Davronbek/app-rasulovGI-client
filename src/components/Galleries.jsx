45cf6e4686b5a448b12267e8e92cbc8824d621da
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-02 10:33:45 +0500

    rasulovGI-app

--- src/components/Galleries.jsx
+++ src/components/Galleries.jsx
@@ -6,8 +6,97 @@ const Galleries = () => {
         <div className="Galleries">
           <div className="container">
             <div className="row">
-              <div className="col-12">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam obcaecati impedit numquam doloribus, ducimus cum! Saepe debitis, ipsum dolores excepturi architecto fugiat eum cumque fugit, id aliquam, sunt nostrum sed.
+              
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/1.jpg" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0 margins">
+                <img src="/img/gallery/2.jpg" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/3.jpg" alt="" className="w-100" />
+              </div>
+              
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/4.png" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0 margins">
+                <img src="/img/gallery/5.png" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/6.png" alt="" className="w-100" />
+              </div>
+              
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/7.png" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0 margins">
+                <img src="/img/gallery/8.png" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/9.jpg" alt="" className="w-100" />
+              </div>
+              
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/10.jpg" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0 margins">
+                <img src="/img/gallery/11.png" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/12.jpg" alt="" className="w-100" />
+              </div>
+              
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/13.jpg" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0 margins">
+                <img src="/img/gallery/14.jpg" alt="" className="w-100" />
+              </div>
+              <div className="col-lg-4 p-0">
+                <img src="/img/gallery/15.jpg" alt="" className="w-100" />
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        <div className="Galleries2">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 mb-4">
+                <div className="cards">
+                  <img className='w-100' src="/img/gallery2/1.jpg"  alt="" />
+                </div>
+              </div>
+              <div className="col-lg-6 mb-4">
+                <div className="cards">
+                  <img className='w-100' src="/img/gallery2/12.jpg"  alt="" />
+                </div>
+              </div>
+              
+              <div className="col-lg-6 mb-4">
+                <div className="cards">
+                  <img className='w-100' src="/img/gallery2/13.jpg"  alt="" />
+                </div>
+              </div>
+              
+              <div className="col-lg-6 mb-4">
+                <div className="cards">
+                  <img className='w-100' src="/img/gallery2/14.jpg"  alt="" />
+                </div>
+              </div>
+              
+              <div className="col-lg-6 mb-4">
+                <div className="cards">
+                  <img className='w-100' src="/img/gallery2/15.jpg"  alt="" />
+                </div>
+              </div>
+              
+              <div className="col-lg-6 mb-4">
+                <div className="cards">
+                  <img className='w-100' src="/img/gallery2/16.jpg"  alt="" />
+                </div>
               </div>
             </div>
           </div>
